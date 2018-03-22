7be76a4cce2dbd629113b810870c9683f7f000f0
Vitaly Dushkin <vetalapo@gmail.com>
2018-03-22 20:55:59 +0300

    added examples for MS Deploy

---
+++ post-build-event-command-line.cmd
@@ -0,0 +1,16 @@
+﻿echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+if $(ConfigurationName) == Debug (
+    echo "Debug"
+    echo "Please start a client at $(ProjectDir)client"
+) else (
+    echo "Release configuration"
+    echo "Building production client...."
+    cd $(ProjectDir)client
+    ng build --prod --env=prod --deploy-url=../wwwroot
+)
+
+echo "Build end." 
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
